99c64f3436106ecd25e9cef673c09eaf1a63aa00
stud0709 <31960113+stud0709@users.noreply.github.com>
2023-02-11 23:41:57 +0100

    list of private keys: selection

--- app/src/main/java/com/onemoresecret/KeyStoreListFragment.java
+++ app/src/main/java/com/onemoresecret/KeyStoreListFragment.java
@@ -1,22 +1,27 @@
 package com.onemoresecret;
 
-import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.selection.ItemDetailsLookup;
+import androidx.recyclerview.selection.ItemKeyProvider;
+import androidx.recyclerview.selection.SelectionPredicates;
+import androidx.recyclerview.selection.SelectionTracker;
+import androidx.recyclerview.selection.StableIdKeyProvider;
+import androidx.recyclerview.selection.StorageStrategy;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 
 import com.onemoresecret.bt.BluetoothController;
 import com.onemoresecret.crypto.CryptographyManager;
 import com.onemoresecret.databinding.FragmentKeyStoreListBinding;
-import com.onemoresecret.databinding.FragmentOutputBinding;
 import com.onemoresecret.databinding.PrivateKeyListItemBinding;
 
 import java.security.KeyStoreException;
@@ -29,55 +34,63 @@ import java.util.Enumeration;
 public class KeyStoreListFragment extends Fragment {
     public static final String TAG = KeyStoreListFragment.class.getSimpleName();
     private FragmentKeyStoreListBinding binding;
+    private SelectionTracker<String> selectionTracker;
+
+    private final CryptographyManager cryptographyManager = new CryptographyManager();
+
+    private String[] aliases;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        try {
+            aliases = new String[cryptographyManager.keyStore.size()];
+            int i = 0;
+            Enumeration<String> aliasesEnum = cryptographyManager.keyStore.aliases();
+            while (aliasesEnum.hasMoreElements()) {
+                aliases[i++] = aliasesEnum.nextElement();
+            }
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
         binding = FragmentKeyStoreListBinding.inflate(inflater, container, false);
         binding.list.setLayoutManager(new LinearLayoutManager(getContext()));
         binding.list.setAdapter(new ItemAdapter());
+
+        selectionTracker = new SelectionTracker.Builder<>("selectionTracker",
+                binding.list,
+                new PrivateKeyItemKeyProvider(ItemKeyProvider.SCOPE_MAPPED),
+                new PrivateKeyLookup(),
+                StorageStrategy.createStringStorage())
+                .withSelectionPredicate(SelectionPredicates.createSelectSingleAnything())
+                .build();
+
+        selectionTracker.addObserver(new SelectionTracker.SelectionObserver<String>() {
+            @Override
+            public void onSelectionChanged() {
+                super.onSelectionChanged();
+
+            }
+        });
+
         return binding.getRoot();
     }
 
-    class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
-        private final CryptographyManager cryptographyManager = new CryptographyManager();
-        private final String[] aliases;
+    class ItemAdapter extends RecyclerView.Adapter<PrivateKeyViewHolder> {
 
-        public ItemAdapter() {
-            try {
-                aliases = new String[cryptographyManager.keyStore.size()];
-                int i = 0;
-                Enumeration<String> aliasesEnum = cryptographyManager.keyStore.aliases();
-                while (aliasesEnum.hasMoreElements()) {
-                    aliases[i++] = aliasesEnum.nextElement();
-                }
-            } catch (KeyStoreException e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
-            }
-        }
 
         @NonNull
         @Override
-        public ItemAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        public KeyStoreListFragment.PrivateKeyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             PrivateKeyListItemBinding binding = PrivateKeyListItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
-            return new ViewHolder(binding);
+            return new KeyStoreListFragment.PrivateKeyViewHolder(binding);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull ItemAdapter.ViewHolder holder, int position) {
-            try {
-                String alias = aliases[position];
-                holder.getBinding().textItemKeyAlias.setText(alias);
-                holder.getBinding().textItemFingerprint.setText(
-                        BluetoothController.byteArrayToHex(
-                                CryptographyManager.getFingerprint(
-                                        (RSAPublicKey) cryptographyManager.getCertificate(alias).getPublicKey())));
-
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        public void onBindViewHolder(@NonNull KeyStoreListFragment.PrivateKeyViewHolder holder, int position) {
+            holder.bind(position);
         }
 
         @Override
@@ -85,17 +98,86 @@ public class KeyStoreListFragment extends Fragment {
             return aliases.length;
         }
 
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            private final PrivateKeyListItemBinding binding;
+    }
 
-            public ViewHolder(@NonNull PrivateKeyListItemBinding binding) {
-                super(binding.getRoot());
-                this.binding = binding;
-            }
 
-            public PrivateKeyListItemBinding getBinding() {
-                return binding;
+    public class PrivateKeyViewHolder extends RecyclerView.ViewHolder {
+        private final PrivateKeyListItemBinding binding;
+        private String alias;
+        private int position;
+
+        public PrivateKeyViewHolder(@NonNull PrivateKeyListItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void bind(int position) {
+            this.position = position;
+            this.alias = aliases[position];
+            try {
+                binding.textItemKeyAlias.setText(alias);
+                binding.textItemFingerprint.setText(
+                        BluetoothController.byteArrayToHex(
+                                CryptographyManager.getFingerprint(
+                                        (RSAPublicKey) cryptographyManager.getCertificate(alias).getPublicKey())));
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+            binding.getRoot().setActivated(selectionTracker.isSelected(alias));
         }
     }
+
+
+    public class PrivateKeyLookup extends ItemDetailsLookup<String> {
+
+        @Nullable
+        @Override
+        public ItemDetails<String> getItemDetails(@NonNull MotionEvent e) {
+            View view = binding.list.findChildViewUnder(e.getX(), e.getY());
+            if (view == null)
+                return null;
+
+            PrivateKeyViewHolder holder = (PrivateKeyViewHolder) binding.list.getChildViewHolder(view);
+            return new ItemDetails<String>() {
+                @Override
+                public int getPosition() {
+                    return holder.position;
+                }
+
+                @Nullable
+                @Override
+                public String getSelectionKey() {
+                    return holder.alias;
+                }
+            };
+        }
+    }
+
+
+    public class PrivateKeyItemKeyProvider extends ItemKeyProvider<String> {
+
+        /**
+         * Creates a new provider with the given scope.
+         *
+         * @param scope Scope can't be changed at runtime.
+         */
+        protected PrivateKeyItemKeyProvider(int scope) {
+            super(scope);
+        }
+
+        @Nullable
+        @Override
+        public String getKey(int position) {
+            return aliases[position];
+        }
+
+        @Override
+        public int getPosition(@NonNull String key) {
+            for (int i = 0; i < aliases.length; i++) {
+                if (aliases[i].equals(key)) return i;
+            }
+            return -1;
+        }
+    }
+
 }
